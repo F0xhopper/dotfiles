dbcbdae14f68f2c2e22bfef8e306c7c2faf567c6
Eden Fox Phillips <foxhopper16@gmail.com>
2026-04-24 09:42:15 +0100

    add checkmate, add auto saving of code

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,13 @@ local highlight_groups = {
   "SignColumn",
 }
 
+vim.opt.autoread = true
+
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "checktime",
+})
+
 for _, group in ipairs(highlight_groups) do
   vim.api.nvim_set_hl(0, group, { bg = "none" })
 end
